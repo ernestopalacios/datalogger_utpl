e8e240632ef265a19022e3862266601eb4bdb81d
ernestopalacios <mecatronica.mid@gmail.com>
2014-09-11 19:40:34 -0500

    Funciona en la Simulacion!

--- PIC_18F452/datV1.0.c
+++ PIC_18F452/datV1.0.c
@@ -23,8 +23,9 @@
 
 
 #include <18f452.h>
-#fuses HS, NOWDT,NOLVP, NOPUT, NOPROTECT
-#use delay(clock=20M)
+#device  ADC=10
+#fuses   HS, NOWDT,NOLVP, NOPUT, NOPROTECT
+#use     delay(clock=20M)
 
 #use RS232(BAUD=9600, UART1, PARITY= N, STREAM = COM_int )
 
@@ -33,43 +34,70 @@
 #include "ds1307.c"
 
 
+#define LED_STATUS PIN_C1
+
 // VARIABLES GLOBALES
 
    unsigned int bufferSerial[100];
-   int i_serial,i_timer, yr, month, day, hrs, min, sec, dow;
-   long valor, valor1;
-
+   int i_serial, i_timer, yr, month, day, hrs, min, sec, dow;
+   long valor, valor1, valor2, valor3, valor4, valor5;
 
 
 
 void main(void)
 {
    enable_interrupts( GLOBAL );
-   setup_adc(ADC_CLOCK_INTERNAL);
    yr=month=day=hrs=min=sec=valor=valor1=0;
    
-   setup_adc(ADC_CLOCK_INTERNAL);
-   ds1307_init();
+   setup_adc(ADC_CLOCK_DIV_32);
+   setup_adc_ports(ALL_ANALOG);
+
+   ds1307_init ();             ///se inicializa el ds1307
+   delay_ms(1000);
+   SET_TRIS_A( 0xFF );
 
    while(1)
    {
       ds1307_get_date (day, month, yr, dow);  /// se obtiene la fecha
       ds1307_get_time (hrs, min, sec);
       
-      output_high(PIN_C1);
-      delay_ms(500);
-      output_low(PIN_C1);
-      delay_ms(50000);     // Retardo de 50seg
-      
-      setup_adc_ports(AN0);   //Solo el canal 0
+      delay_ms(10);
       set_adc_channel(0);
-      set_adc_channel(1);
-      
       valor=read_adc();
+
+      delay_ms(10);
+      set_adc_channel(1);
       valor1=read_adc();
       
+      delay_ms(10);
+      set_adc_channel(2);
+      valor2=read_adc();
+
+      delay_ms(10);
+      set_adc_channel(3);
+      valor3=read_adc();
+
+      delay_ms(10);
+      set_adc_channel(4);
+      valor4=read_adc();
+
+      delay_ms(10);
+      set_adc_channel(5);
+      valor5=read_adc();
+
+      delay_ms(10);
+      
       fprintf(COM_int,"AT$MSGSND=4,\"");
-      fprintf(COM_int, "#PT02,%d%d%d,%d%d%d,%Lu,%Lu,000,000,000,000,000,000,999$\"\n\r", yr,month, day, hrs, min, sec, valor, valor1);
+      fprintf(COM_int, "#PT01,20%d%02d%02d,%02d%02d%02d,%Lu,%Lu,%Lu,%Lu,%Lu,%Lu,000,999$\"\n\r"
+                                yr,month, day, 
+                                          hrs, min, sec, 
+                                                         valor,valor1,valor2,valor3,valor4,valor5);
+
+      output_high( LED_STATUS );
+      delay_ms(1000);
+      output_low( LED_STATUS );
+      
+      delay_ms(5000);
    }
 }
 
